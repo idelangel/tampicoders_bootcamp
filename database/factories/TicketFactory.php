bf920c7c6740c6386112b1880970f4f9e83843e3
Ivens Villalpando <61757532+idelangel@users.noreply.github.com>
2023-07-13 16:27:29 -0600

    Se agrego el codigo de laravel que estamos usando

---
+++ database/factories/TicketFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class TicketFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'subject' => fake()->text(),
+            'details' => fake()->paragraph(),
+            'assigned_to' => fake()->randomNumber(),
+            'priority' => fake()->randomNumber(),
+            'created_at' => now(),
+            'updated_at'    => null,
+        ];
+    }
+}
---
+++ database/migrations/2023_07_13_204427_create_tickets_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->string('subject');
+            $table->text('details');
+            $table->unsignedInteger('assigned_to');
+            $table->integer('priority');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tickets');
+    }
+};
+
